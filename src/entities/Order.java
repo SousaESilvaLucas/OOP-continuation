7c3dee38513bff26df4f0ecad260d7807852c3b3
Lucas Sousa e Silva <sousaesilvalucas@gmail.com>
2019-02-11 11:24:25 -0200

    Enumeration Example

---
+++ src/entities/Order.java
@@ -0,0 +1,44 @@
+package entities;
+
+import java.util.Date;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	
+	private int id;
+	private Date moment;
+	private OrderStatus status;
+	
+	
+	
+	public Order(int id, Date moment, OrderStatus status) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public Date getMoment() {
+		return moment;
+	}
+	
+	public OrderStatus getStatus() {
+		return status;
+	}
+	
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", moment=" + moment + ", status=" + status + "]";
+	}
+	
+	
+
+}
